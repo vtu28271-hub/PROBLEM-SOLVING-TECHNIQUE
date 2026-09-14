be00efc2163c042fd98e24278b1cdc641a1e4a7f
vtu28271-hub <vtu28271@veltech.edu.in>
2026-09-14 19:56:30 +0530

    Add files via upload

---
+++ week2/week2-task5-hashset.java
@@ -0,0 +1,44 @@
+import java.util.HashSet;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Create HashSet
+        HashSet<Integer> set = new HashSet<>();
+
+        // Add elements
+        set.add(10);
+        set.add(20);
+        set.add(30);
+        set.add(20); // Duplicate - will not be added
+
+        System.out.println("HashSet: " + set);
+
+        // Check if element exists
+        System.out.println("Contains 20: " + set.contains(20));
+
+        // Remove element
+        set.remove(10);
+
+        System.out.println("After removing 10: " + set);
+
+        // Size
+        System.out.println("Size: " + set.size());
+
+        // Check if empty
+        System.out.println("Is empty: " + set.isEmpty());
+
+        // Loop through HashSet
+        System.out.println("Elements:");
+
+        for (int num : set) {
+            System.out.println(num);
+        }
+
+        // Clear all elements
+        set.clear();
+
+        System.out.println("After clear: " + set);
+    }
+}
